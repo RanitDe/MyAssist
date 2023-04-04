03abd2dde94c70a9420da2ba9b51917cb76c29f7
Rupaj Sen <rupajps4@gmail.com>
2023-04-04 21:32:22 +0530

    Initial Commit

---
+++ MyAssist/MyAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssistApp.swift
+//  MyAssist
+//
+//  Created by Rupaj Sen on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MyAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
